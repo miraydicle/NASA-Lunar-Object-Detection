12642e6584f77ce2c67f49979c8620b674e5b5d0
Miray Dicle <diclemiray@hotmail.com>
2025-02-18 16:41:20 -0800

    Update train_yolo.py

--- train_yolo.py
+++ train_yolo.py
@@ -3,12 +3,16 @@ from ultralytics import YOLO
 # Load model
 model = YOLO('yolo11n.pt')
 
+# Move model to GPU
+model.to('cuda')
+
 # Train the model with optimized parameters
 model.train(
     data='dataset.yaml',
     epochs=100,
     imgsz=416,
-    workers=4,                # Workers
-    batch=4,                  # Batch size
-    amp=False,                # Mixed precision
+    workers=16,                # Workers
+    batch=128,                  # Batch size
+    amp=True,                # Mixed precision
+    device="cuda"
 )
